96d185e50235233a1e8df8735265d7da2dc7bf86
Kafanakone <kafana.kone@scream.ci>
2021-08-29 01:54:51 +0000

    premiere version du projet

---
+++ script.js
@@ -0,0 +1,50 @@
+window.onload = function(){
+    var canvasWidth = 900;
+    var canvasHeight = 600;
+    var blockSize = 30;
+    var ctx;
+    var delay = 100;
+    var snakee;
+    init();
+
+    function init(){
+        var canvas = document.createElement('canvas')
+        canvas.width = canvasWidth;
+        canvas.height = canvasHeight;
+        canvas.style.border = "1px solid";
+        document.body.appendChild(canvas);
+        ctx = canvas.getContext('2d');
+        snakee = new snake([[6,4], [5,4], [4,4]]);
+        refreshCanvas();
+    }
+    function refreshCanvas(){
+        ctx.clearRect(0,0,canvasWidth, canvasHeight);
+        snakee.draw();
+        snakee.advance();
+        setTimeout(refreshCanvas,delay);
+    }
+    function drawBlock(ctx, position){
+        var x = position[0] * blockSize;
+        var y = position[1] * blockSize;
+        ctx.fillRect(x, y, blockSize, blockSize);
+    }
+    function snake(body){
+        this.body = body;
+        this.draw = function(){
+            ctx.save();
+            ctx.fillStyle = "#ff0000";
+            for(var i = 0; i < this.body.length; i++){
+                drawBlock(ctx, this.body[i]);
+            }
+            ctx.restore();
+        };
+        this.advance = function(){
+            var nextposition = this.body[0].slice();
+            nextposition[0] += 1;
+            this.body.unshift(nextposition);
+            this.body.pop();
+        }
+    }
+    
+    
+}
